e5fd9e32d1df83dadcc77ac242a8eb9f8fad7e86
DrochiczynPoleski <125834172+DrochiczynPoleski@users.noreply.github.com>
2024-01-19 12:36:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function loadPage(page) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("content").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", page, true);
-    xhttp.send();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let parser = new DOMParser();
+      const doc = parser.parseFromString(this.responseText, 'text/html');
+      document.getElementById("content").innerHTML = doc.getElementById("content").innerHTML;
+/* весь body выбирается так:
+      document.getElementById("content").innerHTML = doc.body.innerHTML;
+*/
+    }
+  };
+  xhttp.open("GET", page, true);
+  xhttp.send();
 }
